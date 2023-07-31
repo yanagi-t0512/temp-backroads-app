74551d05c7a733e004f52b0454865615833c1745
yanagi-t0512 <takayama.mhan@gmail.com>
2023-07-31 15:21:45 +0900

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { socialLinks } from '../data';
+import PageLinks from './PageLinks';
+
+const Footer = () => {
+  return (
+    <footer className='section footer'>
+      {/* <ul className='footer-links'>
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className='footer-link'>
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+
+      <PageLinks parentClass={'footer-links'} itemClass={'footer-link'} />
+
+      <ul className='footer-icons'>
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a href={href} target='_blank' rel='noreferrer' className='footer-icon'>
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className='copyright'>
+        copyright &copy; Backroads travel tours company
+        <span id='date'>{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
